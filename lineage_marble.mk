30a8343ff36cb2fd0a4e115bdcf35732a87a9348
Asif <49162687+nhAsif@users.noreply.github.com>
2026-04-23 12:19:04 +0600

    marble: Enable GMS support

--- lineage_marble.mk
+++ lineage_marble.mk
@@ -10,7 +10,7 @@ $(call inherit-product, device/xiaomi/marble/device.mk)
 # Inherit from common lineage configuration
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-EVO_BUILD_TYPE := Official
+WITH_GMS := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_BUILD_APERTURE_CAMERA := false
 TARGET_INCLUDE_ACCORD := false
