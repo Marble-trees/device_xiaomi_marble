89426c7db1938419ccf57e72527df7e583fc896b
nhAsif <najmulhasan3609@gmail.com>
2026-05-04 14:12:48 +0000

    marble: Initialize for Infinity-X

--- infinity_marble.mk
+++ infinity_marble.mk
@@ -7,8 +7,8 @@
 # Inherit from marble device
 $(call inherit-product, device/xiaomi/marble/device.mk)
 
-# Inherit from common lineage configuration
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit from common infinity configuration
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 EVO_BUILD_TYPE := Official
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -16,7 +16,7 @@ TARGET_BUILD_APERTURE_CAMERA := false
 TARGET_INCLUDE_ACCORD := false
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_marble
+PRODUCT_NAME := infinity_marble
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
 
@@ -24,3 +24,12 @@ PRODUCT_MANUFACTURER := Xiaomi
 BUILD_FINGERPRINT := POCO/marble_global/marble:15/AQ3A.250226.002/OS3.0.3.0.VMRMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Maintainer Name
+INFINITY_MAINTAINER := nhAsif
+
+# Whether the device supports Fingerprint On Display
+TARGET_HAS_UDFPS := false
+
+# Whether Including Google Apps
+WITH_GAPPS := true
